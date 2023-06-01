5441459df20a558c6d59f6eda1387834a16eaae9
banna <zhuqingyu@xiaohongshu.com>
2023-06-01 17:34:38 +0800

    feat: examples - vite - react;

---
+++ examples/src/App.tsx
@@ -0,0 +1,46 @@
+import { useState, useRef, memo, useEffect } from 'react';
+import JsonServiceEditorCore from 'json-service-editor';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const { getValue, setValue } = new JsonServiceEditorCore({
+  baseUrl: 'http://localhost:5173',
+  fileName: 'demo.json'
+});
+
+import './App.css';
+
+function App() {
+  const input = useRef(null);
+  const [json, setJson] = useState(() => {
+    getValue().then((res) => {
+      toast.success('获取 JSON 成功!');
+      setJson(JSON.stringify(res));
+    })
+    return '{}';
+  });
+
+  const onChange = () => {
+    const { value } = input.current;
+    setValue(value).then((res) => {
+      toast.success('修改 JSON 成功!');
+      setJson(JSON.stringify(res));
+    })
+  };
+
+  useEffect(() => {
+    if (input.current) input.current.value = json;
+  }, [json]);
+
+  return (
+    <>
+      <div className='container'>
+        <textarea ref={input} className='textarea' />
+        <button onClick={onChange} className='button'>onChange</button>
+      </div>
+      <ToastContainer />
+    </>
+  );
+};
+
+export default memo(App);
---
+++ examples/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/set/demo.json": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: () => {
+          debugger;
+          return `http://localhost:3000/set/demo.json`
+        }
+      },
+      "/get/demo.json": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: () => `http://localhost:3000/get/demo.json`
+      },
+    }
+  }
+})
