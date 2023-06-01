08937a88cca2ea60052a556f09c49584750b2a94
banna <zhuqingyu@xiaohongshu.com>
2023-06-01 15:20:35 +0800

    feat: base;

---
+++ src/index.ts
@@ -0,0 +1,75 @@
+import { JsonServiceEditorOptionType, FetchOptionType, AnyHandle, Methods } from './types';
+
+export const defaultOptions = {
+  baseUrl: 'http://localhost:3000',
+  fileName: ''
+};
+
+class JsonServiceEditorCore {
+  options: JsonServiceEditorOptionType = defaultOptions;
+
+  constructor(options: JsonServiceEditorOptionType | string) {
+    this.options = this.initOptions(options);
+  };
+
+  initOptions = (options) => {
+    if (typeof options === 'string') {
+      return {
+        ...defaultOptions,
+        fileName: options
+      }
+    };
+    return {
+      ...defaultOptions,
+      ...options
+    };
+  };
+
+  fetch = ({ url, method, body, callback, error }: FetchOptionType) => {
+    return fetch(url, {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body
+    })
+      .then((res) => callback(res))
+      .catch((errorMessage) => error(errorMessage))
+  };
+
+  get getFetchOption() {
+    const { options } = this;
+    const { baseUrl, fileName } = options;
+    return {
+      url: `${baseUrl}/get/${fileName}`,
+      method: Methods['GET']
+    };
+  };
+
+  get setFetchOption() {
+    const { options } = this;
+    const { baseUrl, fileName } = options;
+    return {
+      url: `${baseUrl}/get/${fileName}`,
+      method: Methods['POST']
+    };
+  };
+};
+
+class JsonServiceEditor extends JsonServiceEditorCore {
+  constructor(options) {
+    super(options);
+  };
+
+  getValue = ({ callback = () => null, error = () => null } : { callback: AnyHandle, error: AnyHandle }) => {
+    const { getFetchOption } = this;
+    return this.fetch({ ...getFetchOption, callback, error })
+  };
+
+  setValue = ({ body, callback, error } : { callback: AnyHandle, error: AnyHandle, body: any }) => {
+    const { setFetchOption } = this;
+    return this.fetch({ ...setFetchOption, body, callback, error })
+  };
+};
+
+export default JsonServiceEditor;
---
+++ src/types.ts
@@ -0,0 +1,20 @@
+export interface JsonServiceEditorOptionType {
+  baseUrl: string;
+  fileName: string;
+};
+
+export type AnyHandle = (...arg: any[] | any) => any;
+
+export enum Methods {
+  POST = 'POST',
+  GET = 'GET',
+};
+
+// { url, method, body, callback, error }
+export type FetchOptionType = {
+  url: string;
+  method: Methods;
+  body?: any;
+  callback: AnyHandle;
+  error: AnyHandle;
+};
