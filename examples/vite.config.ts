1b16505d74712da3320423008730b477c6b8fc44
banna <zhuqingyu@xiaohongshu.com>
2023-06-08 21:48:54 +0800

    feat: examples;

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    open: true,
     proxy: {
       "/set/demo.json": {
         target: "http://localhost:3000",
